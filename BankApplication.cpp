0e59e4466f01c090e8af77212e9d8e36fb62f53b
Muhannad <101415274+Einooo@users.noreply.github.com>
2022-11-12 20:22:19 +0200

    hello

--- BankApplication.cpp
+++ BankApplication.cpp
@@ -1,5 +1,86 @@
-//
-// Created by muhan on 11/12/2022.
-//
+#include"BankApplication.h"
 
-#include "BankApplication.h"
+double  BankAccount::getBalance() const{
+    return balance;
+}
+void BankAccount::setBalance(double balance){
+    BankAccount::balance = balance;
+}
+void BankAccount:: deposit(){
+    string id;
+    cout << "Enter the account number: ";
+    cin >> id;
+    for (int i = 0; i < clientnum; i++) {
+        if (arr[i].ID == id) {
+            double amount;
+            cout << "Enter the amount to be deposited: ";
+            cin >> amount;
+            arr[i].balance += amount;
+            cout << "Amount deposited successfully!" << endl;
+            cout << "Your new balance is: " << arr[i].balance << endl;
+            break;
+        }
+    }
+}
+void BankAccount:: withdraw(){
+    string id;
+    cout << "Enter the account number: ";
+    cin >> id;
+    for (int i = 0; i < clientnum; i++) {
+        if (arr[i].ID == id) {
+            double amount;
+            cout << "Enter the amount to be withdrawn: ";
+            cin >> amount;
+            if (amount > arr[i].balance) {
+                cout << "Insufficient balance!" << endl;
+            }
+            else {
+                arr[i].balance -= amount;
+                cout << "Amount withdrawn successfully!" << endl;
+                cout << "Your new balance is: " << arr[i].balance << endl;
+            }
+            break;
+        }
+    }
+}
+void BankAccount::display(){
+    for(int i = 0; i < clientnum; i++) {
+        cout << "Name: " << arr[i].name<< endl;
+        cout << "Address: " << arr[i].address << endl;
+        cout << "Phone number: " << arr[i].phonenum << endl;
+        cout << "Type: " << arr[i].type << endl;
+        cout << "Balance: " << arr[i].balance << endl;
+    }
+}
+double SavingBankAccount::getMinimumBalance() const {
+    return minimum_balance;
+}
+
+void  SavingBankAccount::setMinimumBalance(double minimumBalance) {
+    minimum_balance = minimumBalance;
+}
+void Client:: create_account(){
+    cout << "Enter the name of the account holder: ";
+    cin.ignore();
+    getline(cin, name);
+    arr[clientnum].name = name;
+    cout << "Enter the address of the account holder: ";
+    getline(cin, address);
+    arr[clientnum].address = address;
+    cout << "Enter the phone number of the account holder: ";
+    cin >> phonenum;
+    arr[clientnum].phonenum = phonenum;
+    cout << "Enter the type of the account (B/S): ";
+    cin >> type;
+    type = toupper(type);
+    arr[clientnum].type = type;
+    cout << "Enter the initial amount: ";
+    cin >> balance;
+    arr[clientnum].balance = balance;
+    cout << "Account created successfully!" << endl;
+    ID = ID + to_string(clientnum);
+    arr[clientnum].ID = ID;
+    cout << "Your account number is: " << ID << endl;
+    clientnum++;
+    cout << "Press any key to continue..." << endl;
+}
--- BankApplication.h
+++ BankApplication.h
@@ -4,24 +4,61 @@
 
 #ifndef UNTITLED36_BANKAPPLICATION_H
 #define UNTITLED36_BANKAPPLICATION_H
-
-
+#include<bits/stdc++.h>
+using namespace std;
+static int clientnum = 0;
 class BankApplication {
 
 
 };
+class Client;
+class BankAccount {
+private:
+    string ID = "FCAI-";
+    double balance;
+
+public:
+    Client *client;
+
+    double getBalance() const;
+
+    void setBalance(double balance);
+
+    virtual void deposit();
+
+    virtual void withdraw();
+
+    void display();
+
+};
 
 class Client{
+public:
+    BankAccount *account;
+    string name;
+    string address;
+    string phonenum;
+    void create_account();
 
 };
 
-class BankAccount{
 
-};
 class SavingBankAccount:public BankAccount{
+private:
+    double minimum_balance ;
+public:
+    SavingBankAccount(double minimum_balance = 1000){
+        this->minimum_balance = minimum_balance;
+    }
 
+    double getMinimumBalance() const;
+
+    void setMinimumBalance(double minimumBalance);
+    void deposit() override;
+    void withdraw() override;
 };
 
+BankAccount arr[1000];
 
 
 
